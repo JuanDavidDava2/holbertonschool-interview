f89143b2abc2e88b338b17b6a23d4e33de360b31
Juan David Davalos <55117602+JuanDavidDava2@users.noreply.github.com>
2021-01-07 21:53:56 -0500

    min operation

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+min operation
+"""
+
+
+def minOperations(n):
+    """
+    operation
+    """
+    num = n
+    div = 2
+    cont = 0
+
+    if num <= 1:
+        return 0
+
+    while num > 1:
+        if num % div == 0:
+            num = int(num/div)
+            cont += div
+        else:
+            div += 1
+    return cont
+    
