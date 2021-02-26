75e105a364418faef7c5ba781bce82308f99b4ef
Juan David Davalos <55117602+JuanDavidDava2@users.noreply.github.com>
2021-02-25 21:09:24 -0500

    add

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,51 @@
+#include "slide_line.h"
+/**
+ * slide_line - merge and slide an array in a direction
+ * @line: is the array that represent the line
+ * @size: is the size of the line param
+ * @direction: is the direction to merge
+ * Return: 1 in success, 0 in error
+ */
+int slide_line(int *line, size_t size, int direction)
+{
+	size_t pos_find = 0, pos_rep = 0, inc = 1, stop = size;
+	int d_flag = -1;
+
+	if (!line || (direction != SLIDE_LEFT && direction != SLIDE_RIGHT))
+		return (0);
+
+	if (direction == SLIDE_RIGHT)
+	{
+		pos_find = size - 1;
+		pos_rep = size - 1;
+		inc = -1;
+		stop = -1;
+	}
+
+	while (pos_find != stop)
+	{
+		if (*(line + pos_find) != 0)
+		{
+			if (d_flag == *(line + pos_find))
+			{
+				*(line + pos_find) += d_flag;
+				d_flag = -1;
+			}
+			else
+			{
+				d_flag = *(line + pos_find);
+				if ((direction == SLIDE_LEFT && pos_find != 0)
+						||
+						((direction == SLIDE_RIGHT && pos_find != (size - 1))))
+					if (*(line + pos_rep) != 0)
+						pos_rep += inc;
+			}
+			*(line + pos_rep) = *(line + pos_find);
+			if (pos_rep != pos_find)
+				*(line + pos_find) = 0;
+		}
+		pos_find += inc;
+	}
+
+	return (1);
+}
